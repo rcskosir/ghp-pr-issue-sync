b21762a80cd33a95d80975c24ea1821593a87e31
rcskosir <renae.kosir@hashicorp.com>
2023-08-18 10:53:55 -0400

    Repo is a String not an array since I only need one repo, removing authors for issues

--- cli/flags-issues.go
+++ cli/flags-issues.go
@@ -2,8 +2,6 @@ package cli
 
 import (
 	"fmt"
-	"strings"
-
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -12,12 +10,11 @@ type FlagDataIssues struct {
 	Token         string
 	Org           string
 	Owner         string
-	Repos         []string
+	Repo          string
 	ProjectNumber int
-	Authors       []string
 }
 
-// May need to have 2 configure flags? one for PR project, one for issue project
+// Pulling from one repo, so Repo is a String, not concerned with Authors for Issues
 func configureFlagsIssues(root *cobra.Command) error {
 	flags := FlagDataIssues{}
 	pflags := root.PersistentFlags()
@@ -25,9 +22,8 @@ func configureFlagsIssues(root *cobra.Command) error {
 	pflags.StringVarP(&flags.Token, "token", "t", "", "github oauth token (GITHUB_TOKEN)")
 	pflags.StringVarP(&flags.Org, "org", "o", "", "github organization (GITHUB_ORG)") // nolint: misspell
 	pflags.StringVarP(&flags.Owner, "owner", "", "", "github repo owner, defaults to org (GITHUB_OWNER)")
-	pflags.StringSliceVarP(&flags.Repos, "repo", "r", []string{}, "github repo name (GITHUB_REPO) or a set of repos `repo1,repo2`")
+	pflags.StringVarP(&flags.Repo, "repo", "r", "", "github repo name (GITHUB_REPO)")
 	pflags.IntVarP(&flags.ProjectNumber, "project-number", "p", 0, "github project number (GITHUB_PROJECT_NUMBER)")
-	pflags.StringSliceVarP(&flags.Authors, "authors", "a", []string{}, "only sync prs by these authors. ie 'katbyte,author2,author3'")
 
 	// binding map for viper/pflag -> env
 	m := map[string]string{
@@ -60,23 +56,12 @@ func GetFlagsIssues() FlagDataIssues {
 		owner = viper.GetString("org")
 	}
 
-	// TODO BUG for some reason it is not correctly splitting on ,? so hack this in
-	authors := viper.GetStringSlice("authors")
-	if len(authors) > 0 {
-		authors = strings.Split(authors[0], ",")
-	}
-	repos := viper.GetStringSlice("repo")
-	if len(repos) > 0 {
-		repos = strings.Split(repos[0], ",")
-	}
-
 	// there has to be an easier way....
 	return FlagDataIssues{
 		Token:         viper.GetString("token"),
 		Org:           viper.GetString("org"),
 		Owner:         owner,
-		Repos:         repos,
+		Repo:          viper.GetString("repo"),
 		ProjectNumber: viper.GetInt("project-number"),
-		Authors:       authors,
 	}
 }
