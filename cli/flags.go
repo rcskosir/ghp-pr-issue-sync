cbe4a9bdec06635b1b8336b8ad5ebc89d2e6d3c2
rcskosir <renae.kosir@hashicorp.com>
2023-10-25 13:26:23 -0500

    adding labels as an environment variable and updating flags to expect the env var

--- cli/flags.go
+++ cli/flags.go
@@ -38,7 +38,7 @@ func configureFlags(root *cobra.Command) error {
 		"repo":           "GITHUB_REPO", // todo rename this to repos
 		"project-number": "GITHUB_PROJECT_NUMBER",
 		"authors":        "GITHUB_AUTHORS",
-		"labels":         "",
+		"labels":         "GITHUB_LABELS",
 	}
 
 	for name, env := range m {
